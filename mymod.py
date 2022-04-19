d95c29b10f2f5e0d74aa7c545fbaec19155f1e37
Niko P <nikop.@Nikos-MacBook-Pro.local>
2022-04-18 18:08:44 -0700

    first commit

---
+++ mymod.py
@@ -0,0 +1,16 @@
+#def sayHello():
+    #print("Hello")
+    
+def footage(length, width):
+    sqft = length * width
+    return sqft
+ 
+    
+    
+def Circumference():
+    
+    radius = input("what is the diameter?")
+    circumference= (radius * 2 * 3.14)
+    return circumference    
+
+    
---
+++ mymod1.py
@@ -0,0 +1,5 @@
+def Circumference():
+    
+    diameter = input("what is the diameter?")
+    circumference= (diameter * 3.14)
+    return circumference
